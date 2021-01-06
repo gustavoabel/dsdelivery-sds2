5d561dcc490889acaa2fe17027774a17236fffdb
gustavo abel <gustavoabel.contato@gmail.com>
2021-01-06 10:44:25 -0300

    GET /orders

--- backend/src/main/java/com/devsuperior/dsdeliver/entities/Order.java
+++ backend/src/main/java/com/devsuperior/dsdeliver/entities/Order.java
@@ -13,30 +13,29 @@ import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.Table;
+
 @Entity
-@Table( name = "tb_order")
-
-public class Order implements Serializable{
-
+@Table(name = "tb_order")
+public class Order implements Serializable {
+	
 	private static final long serialVersionUID = 1L;
 	
 	@Id
-	@GeneratedValue(strategy = 	GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String address;
-	private Double latitude;
+	private Double latitude;	
 	private Double longitude;
 	private Instant moment;
 	private OrderStatus status;
 	
-    @ManyToMany
-    @JoinTable(name = "tb_order_product",
-    joinColumns = @JoinColumn(name = "order_id"),
-    inverseJoinColumns = @JoinColumn(name = "product_id"))
+	@ManyToMany
+	@JoinTable(name = "tb_order_product", 
+		joinColumns = @JoinColumn(name = "order_id"),
+		inverseJoinColumns = @JoinColumn(name = "product_id"))
 	private Set<Product> products = new HashSet<>();
-	
-	public Order () {
-		
+
+	public Order() {
 	}
 
 	public Order(Long id, String address, Double latitude, Double longitude, Instant moment, OrderStatus status) {
@@ -125,7 +124,4 @@ public class Order implements Serializable{
 			return false;
 		return true;
 	}
-
-
-	
 }
